4242598c97fb5511c42e047d067cb521573f167e
mendesthi <thiagocm@gmail.com>
2019-11-13 11:59:48 +0100

    Issues with sharp module - removed

--- modules/awsS3.js
+++ modules/awsS3.js
@@ -1,98 +1,84 @@
 /* AWS S3 module to temporarily store pictures on S3 bucket, and delete them */
 /* ... parameters set in environment variables */
 
-/** Environment Variables Required:
- *
+/** Environment Variables Required: 
+ * 
  * AWS_BUCKET           - S3 Bucket ID
  * AWS_REGION           - AWS Region
  * AWS_ACCESS_KEY       - AWS Access Key
  * AWS_SECRET_KEY       - AWS Secret Key
- *
+ * 
  * */
 
-var AWS = require("aws-sdk");
-const fs = require("fs");
-const sharp = require("sharp");
-const path = require("path");
+var AWS = require('aws-sdk')
+const fs = require('fs')
 
-const BUCKET = process.env.AWS_BUCKET;
-const REGION = process.env.AWS_REGION;
-const ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-const SECRET_KEY = process.env.AWS_SECRET_KEY;
+const BUCKET = process.env.AWS_BUCKET
+const REGION = process.env.AWS_REGION
+const ACCESS_KEY = process.env.AWS_ACCESS_KEY
+const SECRET_KEY = process.env.AWS_SECRET_KEY
 
-const imageRemoteName = `selfie_${new Date().getTime()}.png`;
+const imageRemoteName = `selfie_${new Date().getTime()}.png`
 
 module.exports = {
-  UploadImage: function(userImage, response) {
-    return UploadImage(userImage, response);
-  },
-  DeleteImage: function(userImage, response) {
-    return DeleteImage(userImage, response);
-  }
-};
+    UploadImage: function (userImage, response) {
+            return (UploadImage(userImage, response));
+        },
+    DeleteImage: function (userImage, response) {
+        return (DeleteImage(userImage, response));
+    }
+}
 
 function UploadImage(userImage, callback) {
-  AWS.config.update({
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_KEY,
-    region: REGION
-  });
+    AWS.config.update({
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_KEY,
+        region: REGION
+    })
 
-  var s3 = new AWS.S3();
-  buf = new Buffer(userImage, "base64");
+    var s3 = new AWS.S3()
+    buf = new Buffer(userImage,'base64')
 
-  sharp(buf)
-    .composite([
-      {
-        input: path.join(__dirname, "../static/resources/superhero_frame.png"),
-        gravity: "southeast"
-      }
-    ])
-    .toBuffer()
-    .then(function(outputBuffer) {
-      s3.putObject({
+    s3.putObject({
         Bucket: BUCKET,
-        Body: outputBuffer,
+        Body: buf,
         Key: imageRemoteName,
         ACL: "public-read",
-        ContentType: "image/png"
-      })
+        ContentType: 'image/png'
+    })
         .promise()
         .then(response => {
-          console.log(`done! - `, response);
-          console.log(
-            `The URL is ${s3.getSignedUrl("getObject", {
-              Bucket: BUCKET,
-              Key: imageRemoteName
-            })}`
-          );
+        console.log(`done! - `, response)
+        console.log(
+            `The URL is ${s3.getSignedUrl('getObject', { Bucket: BUCKET, Key: imageRemoteName })}`
+        )
 
-          response = `${s3.getSignedUrl("getObject", {
-            Bucket: BUCKET,
-            Key: imageRemoteName
-          })}`;
-          callback(null, response);
+        response = `${s3.getSignedUrl('getObject', { Bucket: BUCKET, Key: imageRemoteName })}`
+        callback(null, response);
         })
         .catch(err => {
-          console.log("failed:", err);
-        });
-    });
+        console.log('failed:', err)
+        }
+    )
 }
 
 function DeleteImage(imageToDelete, callback) {
-  AWS.config.update({
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_KEY,
-    region: REGION
-  });
+    AWS.config.update({
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_KEY,
+        region: REGION
+    })
 
-  var s3 = new AWS.S3();
-  var params = { Bucket: BUCKET, Key: imageToDelete };
+    var s3 = new AWS.S3()
+    var params = {Bucket: BUCKET, Key: imageToDelete};
 
-  s3.deleteObject(params, function(err, data) {
-    if (!err) console.log("Image deleted from S3: " + data);
-    else console.log(err, err.stack);
-    response = data;
-    callback(null, response);
-  });
+    s3.deleteObject(params, function(err, data) {
+        if (!err) 
+            console.log("Image deleted from S3: " + data);
+
+        else
+            console.log(err, err.stack);  
+            response = data;
+            callback(null, response);    
+    });
 }
